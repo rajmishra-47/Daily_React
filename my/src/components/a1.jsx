681e8aee9bba5fe0cd141241a6feae41ef75c415
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-21 11:26:05 +0530

    worin g with components

---
+++ my/src/components/a1.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function A1() {
+  return (
+    <div>
+        hello,my self Raj.This is my first component
+        <p className="text-center m-5">This is another tex with bootstrap included.</p>
+
+    </div>
+  )
+}
