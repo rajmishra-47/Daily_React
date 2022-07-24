4db14b666424a7b0352a26dd7bbfd2f824d19e70
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-24 10:35:41 +0530

    changes in props

--- my/src/App.js
+++ my/src/App.js
@@ -6,7 +6,7 @@ import Counter from './components/counter';
 function App() {
   return (
     <>
-    <A1 name="hello from props side"></A1>
+    <A1 name="Raj"></A1>
     <Counter></Counter>
     </>
   );
--- my/src/components/a1.jsx
+++ my/src/components/a1.jsx
@@ -6,7 +6,7 @@ export default function A1(props) {
         {/* hello,my self Raj.This is my first component
         <p className="text-center m-5">This is another tex with bootstrap included.</p> */}
 
-        <h1>{props.name}</h1>
+        <h1>hello {props.name}</h1>
     </div>
   )
 }
