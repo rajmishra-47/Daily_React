7e49e0122944037cb343daa2980f4cf4d1714f33
RAJ Mishra <rajnandanmishra2002@gmail.com>
2022-07-31 23:54:39 +0530

    pasing data through props

--- my/src/components/d1.jsx
+++ my/src/components/d1.jsx
@@ -1,12 +1,19 @@
 import React from 'react'
 import './d11.css'
-export default function D1() {
+export default function D1(props) {
+  
+  const c=(a,b)=>{
+    return (a+b);
+  }
+
+  const date=new Date();
   return (
     <>
-
     <div id="a1">
         hello
     </div>
+
+    <h1>{(parseInt(props.a)+parseInt(props.b))}</h1>
     </>
   )
 }
